6dc9050374161ce4f7fb57d9a2fb34eec1f886bd
riccardofano <rcr.fano@gmail.com>
2023-03-12 19:42:19 +0100

    add auto-session

---
+++ lua/custom/plugins/autosession.lua
@@ -0,0 +1,10 @@
+-- Restore last buffers when opening nvim
+return {
+  'rmagatti/auto-session',
+  config = function()
+    require("auto-session").setup {
+      log_level = "error",
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    }
+  end
+}
