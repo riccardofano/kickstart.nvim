b31b10114d7d76e1cc2bfba332789315aa6dc93a
riccardofano <rcr.fano@gmail.com>
2023-03-19 20:03:20 +0100

    always enable relative line numbers and split below

--- lua/riccardo/set.lua
+++ lua/riccardo/set.lua
@@ -1,14 +1,17 @@
 -- change language to English
 vim.cmd("language en_UK")
 
--- enable ctrl-v
-vim.cmd("source $VIMRUNTIME/mswin.vim")
+-- relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- only show one statusline
 vim.opt.laststatus = 3
 
 -- open new splits to the right instead of left
 vim.opt.splitright = true
+-- open new splits to below instead of above
+vim.opt.splitbelow = true
 
 vim.opt.scrolloff = 8
 
